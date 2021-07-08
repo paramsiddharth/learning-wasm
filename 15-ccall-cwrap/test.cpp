24eb012d941aa020a12e058895a60d2d2e55db18
Param Siddharth <contact@paramsid.com>
2021-07-09 00:07:51 +0530

    Use ccall and cwrap

---
+++ 15-ccall-cwrap/test.cpp
@@ -0,0 +1,25 @@
+#include <emscripten/emscripten.h>
+#include <iostream>
+
+extern "C" {
+	int EMSCRIPTEN_KEEPALIVE add(int a, int b) {
+		return a + b;
+	}
+	
+	long long EMSCRIPTEN_KEEPALIVE fibonacci(int n) {
+		if (n < 2)
+			return n;
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+	void EMSCRIPTEN_KEEPALIVE sayHello() {
+		std::cout << "Hello, World!" << std::endl;
+	}
+
+	int EMSCRIPTEN_KEEPALIVE arraySum(int* arr, int size) {
+		int net = 0;
+		for (int i = 0; i < size; i++)
+			net += arr[i];
+		return net;
+	}
+}
